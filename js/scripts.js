00848e750ebe951024d3ba9c8aac6d6e0c2ca8fd
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-26 13:11:42 -0800

    fixed front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,18 @@ Pizza.prototype.newOrder = function() {
     this.price = this.price + 5;
     return
   }
-  // if(this.type === 2){
-  //   this.price + 7;
-  //   return
-  // }
+  if(this.type === 2){
+    this.price = this.price + 7;
+    return
+  }
 }
 
 $(document).ready(function() {
   var customer = new Pizza();
-  $("button#order").click(function(event) {
-  $("input:checkbox[name=pizza]:checked").each(function(event){
+  $("form#order").submit(function(event) {
+  $("input:checkbox[name=pizza]:checked").val();
     customer.newOrder();
     $("#price").append(customer.price);
-  });
+
 });
 })
