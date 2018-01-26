f8376288a0bb82cef247b1cd1bdbe35a50d86cf0
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-26 13:38:38 -0800

    add full functionality, and $ sign

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 function Pizza(){
   this.type = '';
+  this.top = '';
   this.price = 0;
 }
 
 Pizza.prototype.sizeType = function(pizzaType) {
   this.type = pizzaType
-  if (this.type === '1') {
+  if(this.type === '1') {
     this.price = this.price + 5;
   }
-  if (this.type === '2') {
+  if(this.type === '2') {
     this.price = this.price + 7;
+  }
+}
+
+Pizza.prototype.pickTop = function(pizzaTop) {
+  this.top = pizzaTop
+  if(this.top === '3') {
+      this.price = this.price + 3;
+  }
+  if(this.top === '4') {
+    this.price = this.price + 1;
   }
 }
 
@@ -19,6 +30,7 @@ $(document).ready(function() {
     customer.price = 0
     $("input:checkbox[name=pizza]:checked").each(function(index, checkbox){
       customer.sizeType(checkbox.value);
+      customer.pickTop(checkbox.value);
     });
     $("#price").text(customer.price);
   });
